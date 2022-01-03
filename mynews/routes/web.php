cf93b66f13e4e6ec8e749f20afdcdd04479ac2bf
masa <ao@gmail.com>
2022-01-03 15:43:31 +0000

    first commit

--- mynews/routes/web.php
+++ mynews/routes/web.php
@@ -14,10 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
-});
-Route::get('XXX', 'XXX\AAAController@bbb');
